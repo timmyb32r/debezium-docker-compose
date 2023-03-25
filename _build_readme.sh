abdf756d0f458ad6b5a787b3414a27811b28ebe1
Timofey Brunko <timmyb32r@yandex-team.ru>
2023-03-25 23:01:23 +0300

    simplify navigation

--- _build_readme.sh
+++ _build_readme.sh
@@ -3,4 +3,4 @@ echo ''>./README.md
 echo '# debezium-docker-compose' >>./README.md
 echo 'demo cases:' >>./README.md
 
-ls -1d */ | awk '{print("* ",$1)}' | rev | cut -c2- | rev >>./README.md
+ls -1d */ | rev | cut -c2- | rev | awk -F\, '{printf("* [%s](https://github.com/timmyb32r/debezium-docker-compose/%s)\n",$1,$1)}' >>./README.md
